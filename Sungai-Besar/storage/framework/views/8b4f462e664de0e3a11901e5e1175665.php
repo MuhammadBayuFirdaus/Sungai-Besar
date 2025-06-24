aca8e06ac6bdbebdbf2c7334f677d6df6c939825
MuhammadBayuFirdaus <aselloutbayufirdaus@gmail.com>
2025-06-25 07:44:33 +0800

    first commit

---
+++ Sungai-Besar/storage/framework/views/8b4f462e664de0e3a11901e5e1175665.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo e(asset('css/navbar.css')); ?>">
+    <title>Document</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="<?php echo e(asset('img/logo Sungai Besar Panjang.png')); ?>" alt="Logo" class="logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Berita Terkini</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kegiatan</a>
+          </li>
+        </ul>
+      <div class="d-flex">
+        <a href="#" class="btn me-2">Sign In</a>
+        <a href="#" class="btn">Sign Up</a>
+      </div>
+      </div>
+    </div>
+  </nav>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\Sungai Besar\Sungai-Besar\resources\views/navbar.blade.php ENDPATH**/ ?>
